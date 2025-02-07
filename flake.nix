154f8e4d34a19fc30c6c6fdbadb5c207a7981319
Jan Olencki <jolencki@antmicro.com>
2025-02-07 12:30:24 +0100

    Enable Stylix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,18 @@
 {
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
-    # Home manager
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    stylix.url = "github:danth/stylix/release-24.11";
     helix.url = "github:helix-editor/helix";
   };
 
   outputs = {
     nixpkgs,
     home-manager,
+    stylix,
     helix,
     ...
   } @ inputs: let
@@ -36,6 +36,8 @@
     homeConfigurations."root" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
       modules = [
+        stylix.homeManagerModules.stylix
+        stylixModules.no-gui
         homeConfigurationModules.basic-no-gui
         {
           home.username = "root";
