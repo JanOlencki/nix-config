397ce786d6c766c8e6a89de68bef527ad5bd6d68
Jan Olencki <jan@olencki.pl>
2025-06-03 20:25:26 +0200

    Fix warnings

--- common/home-main.nix
+++ common/home-main.nix
@@ -15,32 +15,34 @@
   };
   programs.vscode = {
     enable = true;
-    keybindings = [
-      {
-        "key" = "alt+k";
-        "command" = "workbench.action.nextEditorInGroup";
-      }
-      {
-        "key" = "alt+j";
-        "command" = "workbench.action.previousEditorInGroup";
-      }
-    ];
-    userSettings = {
-      "editor.codeLens" = false;
-      "diffEditor.ignoreTrimWhitespace" = false;
-      "editor.autoClosingBrackets" = "never";
-      "editor.autoClosingOvertype" = "never";
-      "editor.autoClosingQuotes" = "never";
-      "editor.autoClosingDelete" = "never";
-      "editor.acceptSuggestionOnCommitCharacter" = false;
-      "editor.acceptSuggestionOnEnter" = "off";
-      "editor.autoSurround" = "never";
-      "editor.dragAndDrop" = false;
-      "editor.minimap.enabled" = false;
-      "editor.folding" = false;
-      "editor.foldingHighlight" = false;
-      "extensions.ignoreRecommendations" = true;
-      "files.autoSave" = "onWindowChange";
+    profiles.default = {
+      keybindings = [
+        {
+          "key" = "alt+k";
+          "command" = "workbench.action.nextEditorInGroup";
+        }
+        {
+          "key" = "alt+j";
+          "command" = "workbench.action.previousEditorInGroup";
+        }
+      ];
+      userSettings = {
+        "editor.codeLens" = false;
+        "diffEditor.ignoreTrimWhitespace" = false;
+        "editor.autoClosingBrackets" = "never";
+        "editor.autoClosingOvertype" = "never";
+        "editor.autoClosingQuotes" = "never";
+        "editor.autoClosingDelete" = "never";
+        "editor.acceptSuggestionOnCommitCharacter" = false;
+        "editor.acceptSuggestionOnEnter" = "off";
+        "editor.autoSurround" = "never";
+        "editor.dragAndDrop" = false;
+        "editor.minimap.enabled" = false;
+        "editor.folding" = false;
+        "editor.foldingHighlight" = false;
+        "extensions.ignoreRecommendations" = true;
+        "files.autoSave" = "onWindowChange";
+      };
     };
   };
   programs.wezterm = {
