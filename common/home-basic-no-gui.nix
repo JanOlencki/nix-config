f37aab8ff3db5395b97f2d63f3c0078709b89fb6
Jan Olencki <jan@olencki.pl>
2025-11-13 19:56:11 +0100

    Improve Zellij

--- common/home-basic-no-gui.nix
+++ common/home-basic-no-gui.nix
@@ -211,11 +211,12 @@
     enable = true;
     settings = {
       show_startup_tips = false;
+      advanced_mouse_actions = false;
       keybinds.locked = {
         "bind \"Alt h\"" = {MoveFocusOrTab = "Left";};
         "bind \"Alt l\"" = {MoveFocusOrTab = "Right";};
-        "bind \"Alt j\"" = {MoveFocusOrTab = "Down";};
-        "bind \"Alt k\"" = {MoveFocusOrTab = "Up";};
+        "bind \"Alt j\"" = {MoveFocus = "Down";};
+        "bind \"Alt k\"" = {MoveFocus = "Up";};
       };
     };
   };
