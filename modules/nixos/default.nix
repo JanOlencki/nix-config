5a15a5b6c41aff7360fa793100c592c53d029993
Jan Olencki <jolencki@antmicro.com>
2026-06-18 17:23:21 +0200

    Add vial and some minor packages, remove substituters

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -21,12 +21,8 @@
       "flakes"
     ];
     extra-substituters = [
-      "https://helix.cachix.org"
-      "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };
 
@@ -54,6 +50,18 @@
       };
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    sbctl
+    exfat
+    usbutils
+    vial
+  ];
+
+  services.udev.packages = [
+    pkgs.via
+    pkgs.vial
+  ];
 
   # Printing
   services.printing.enable = true;
